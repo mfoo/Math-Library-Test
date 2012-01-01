f2e1c8a947800ed7edd0cb75ad02fb3ef6892277
Martin Foot <martin@mfoot.com>
2012-01-01 18:27:02 +0000

    Add Mother of All Random Number Generator.

---
+++ src/mother.h
@@ -0,0 +1,14 @@
+/**
+ * Declaration of the Mother of All Random Number Generator by George Marsaliga
+ * defined in mother.c. mother.c can be found at
+ * ftp://ftp.forth.org/pub/C/mother.c
+ */
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+double Mother(unsigned long *pSeed);
+
+#ifdef __cplusplus
+}
+#endif
